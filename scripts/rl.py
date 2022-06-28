12634fd8ff0d87147c556a8d5de0c1c6adbcf106
Eshaan Agrawal <eshaan@honglab2.cs.uga.edu>
2022-06-28 15:18:37 -0400

    Updated gitignore

--- scripts/rl.py
+++ scripts/rl.py
@@ -39,7 +39,7 @@ from algo.ppo.ppo import Dec_Train
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='PPO forward reinforcement learning')
-    parser.add_argument('--env', type=str, default='ma_gym:Checkers-v0', help='Provide the env')
+    parser.add_argument('--env', type=str, default='ma_gym:DecHuRoSorting-v0', help='Provide the env')
     parser.add_argument('--training_epochs', type=int, default=100, help='Total training epochs')
     args = parser.parse_args()
 
